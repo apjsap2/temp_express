fec4036337c7fbcd78813a2c2837b7db75205342
종서최 <jongseochoe@jongseochoeui-iMac.local>
2022-11-29 01:23:51 +0900

    몽고DB Atlas connection string 작업완료. 이제 코멘트에 대해 schema 작업 하면 될듯

--- index.js
+++ index.js
@@ -5,13 +5,23 @@ const ejsMate = require('ejs-mate')
 const PORT = process.env.PORT || 3000;
 
 
+//   이하 mongoDB Atlas 연결 관련 코드
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://apjsap:!ab1988127@cluster0.gcuka.mongodb.net/?retryWrites=true&w=majority')
+	.then(() => {
+		console.log('MONGO CONNECTION OPEN!!')
+	})
+	.catch(err => {
+		console.log('OH NO, mongo ERROR!!')
+		console.log(err)
+	})
+
+//   이상 mongoDB Atlas 연결 관련 코드
+
 
 //   이하  local-data.js에서 더미 정보 가져오기
-
 const {articles} = require('./local-data.js')
 const {comments} = require('./local-data.js')
-
-
 //   이상  local-data.js에서 더미 정보 가져오기
 
 
