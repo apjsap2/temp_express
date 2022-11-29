64b52087c4b4c437789d1477c64ccb48e1b36c22
apjsap <apjsap2@naver.com>
2022-11-29 14:16:21 +0900

    스키마 정의 직전의 커밋

--- index.js
+++ index.js
@@ -6,15 +6,16 @@ const PORT = process.env.PORT || 3000;
 
 
 //   이하 mongoDB Atlas 연결 관련 코드
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://apjsap:!ab1988127@cluster0.gcuka.mongodb.net/?retryWrites=true&w=majority')
-	.then(() => {
-		console.log('MONGO CONNECTION OPEN!!')
-	})
-	.catch(err => {
-		console.log('OH NO, mongo ERROR!!')
-		console.log(err)
-	})
+// mongoose.connect('mongodb+srv://apjsap:!ab1988127@cluster0.gcuka.mongodb.net/?retryWrites=true&w=majority')
+// 	.then(() => {
+// 		console.log('MONGO CONNECTION OPEN!!')
+// 	})
+// 	.catch(err => {
+// 		console.log('OH NO, mongo ERROR!!')
+// 		console.log(err)
+// 	})
 
 //   이상 mongoDB Atlas 연결 관련 코드
 
@@ -35,6 +36,11 @@ app.get('/', (req, res) => {
 	res.render('index', {cssUrl})
 })
 
+
+app.get('/apps/comments/new', (req, res) => {
+	const cssUrl = '/css/new.css'
+	res.render('comments/new', {cssUrl})
+})
 
 app.get('/apps/comments', (req, res) => {
 	const cssUrl = '/css/comments.css'
